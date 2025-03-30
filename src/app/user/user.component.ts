fdf01bbe8a55f388e163eb01cfc0274789975058
balrampu21 <140879963+balrampu21@users.noreply.github.com>
2025-03-30 14:45:24 +0530

    type , interface , undefined values
    type , interface , undefined values

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,16 @@
 import { Component , EventEmitter, Input , Output, output , computed,input } from '@angular/core';
+// type User={
+//     id: string;
+//     avatar : string;
+//     name : string;
+// }
 
+interface   User {
+       id: string;
+       avatar : string;
+       name : string;
+   }
+   
 @Component({
   selector: 'app-user',
   imports: [],
@@ -7,14 +18,20 @@ import { Component , EventEmitter, Input , Output, output , computed,input } fro
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-   @Input({required: true}) id! : string;
-  @Input({required : true}) avatar! : string;
-  @Input({required : true}) name! : string;
+  // @Input({required: true}) id! : string;
+  // @Input({required : true}) avatar! : string;
+  // @Input({required : true}) name! : string;
+  // @Input({required : true}) user! : {
+  //   id: string;
+  //   avatar : string;
+  //   name : string;
+  // };
 
+  @Input({required : true}) user! : User;
   // avatar = input.required<string>();
   // name = input.required<string>();
   get imagePath(){
-    return "assets/users/users/" + this.avatar;
+    return "assets/users/users/" + this.user.avatar;
   }
 
   //  imagePath= computed( ()=>{
@@ -25,9 +42,9 @@ export class UserComponent {
   @Output() selectName = new EventEmitter();
             selectAvtar = output<string>();
     onSelectUser(){
-      this.select.emit(this.id);
-      this.selectName.emit(this.name);
-      this.selectAvtar.emit(this.avatar);
+      this.select.emit(this.user.id);
+      this.selectName.emit(this.user.name);
+      this.selectAvtar.emit(this.user.avatar);
       
     }
 
