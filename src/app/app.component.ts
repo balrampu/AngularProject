6a605c046462849803f9200e5d044cb57e3f3a49
balrampu21 <140879963+balrampu21@users.noreply.github.com>
2025-03-30 22:37:46 +0530

    legacy ngfor and ngif under angular 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,10 @@ import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
 import { TasksComponent } from './tasks/tasks.component';
+import { NgFor,NgIf } from '@angular/common';
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent,UserComponent,TasksComponent],
+  imports: [HeaderComponent,UserComponent,TasksComponent,NgFor,NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
